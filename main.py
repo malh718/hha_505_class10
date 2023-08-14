04d3fc46e3117bfee5f840762c2433d8198bd7bc
maliha <you@example.com>
2023-08-14 14:41:16 +0000

    more examples

---
+++ main.py
@@ -0,0 +1,10 @@
+from module1 import addition, subtraction
+
+add = addition
+sub = subtraction
+
+output1 =  addition (5, 10)
+output2 =  subtraction (5,10)
+
+print ('Output 1:', output1)
+print ('Output 2:', output2)
